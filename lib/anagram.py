65e8eccbfa9603a9a278a2bf13a64309e1eea7d1
Hana Sambora <grace.sambora@gmail.com>
2023-12-04 14:26:00 -0600

    Complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,23 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list_poss):
+        orig_letters = []
+        for letter in self.word :
+            orig_letters.append(letter)
+        #print(orig_letters)
+
+        match_words = []
+        for poss_word in list_poss:
+            poss_word_letters = []
+            for letter in poss_word:
+                poss_word_letters.append(letter)
+            if sorted(orig_letters) == sorted(poss_word_letters):
+                match_words.append(poss_word)
+        
+        #print(match_words)
+        return match_words
+
+#Anagram("listen").match(["inlets", "goodbye", "hello"])
